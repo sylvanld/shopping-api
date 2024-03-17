6c5b741c68c68b4d5fd46a041eeb7a130a1d503b
Gitub Action <action@github.com>
2024-03-17 01:03:39 +0000

    Release version 0.1.0

--- shopping/api/metadata.py
+++ shopping/api/metadata.py
@@ -1,6 +1,6 @@
 API_TITLE = "Shopping API"
 API_DESCRIPTION = "API that expose features to collaborate on shopping lists."
-API_VERSION = "X.Y.Z"
+API_VERSION = "0.1.0"
 API_TAGS = [
     {"name": "api", "description": "Endpoints to get info about current api instance."},
     {"name": "carts", "description": "Endpoints to create and manage carts."},
