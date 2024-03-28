ee6b1097f822916701bca892e52bd85dcba00437
Sylvan Le Deunff <sylvan.le-deunff@ovhcloud.com>
2024-03-28 23:03:05 +0100

    Add endpoint to check ingredient in shopping API

--- shopping/api/v1/cart.py
+++ shopping/api/v1/cart.py
@@ -1,6 +1,8 @@
+from typing import List
+
 from fastapi import APIRouter, Path
 
-from shopping.api.v1.dtos import CartDTO
+from shopping.api.v1.dtos import CartDTO, CartItemAddDTO
 from shopping.service.cart import CartService
 
 router = APIRouter(tags=["cart"])
@@ -10,3 +12,13 @@ cart_service = CartService()
 @router.get("/groups/{groupUID}/cart", response_model=CartDTO)
 async def get_cart_details(group_uid: str = Path(..., alias="groupUID")):
     return cart_service.get_cart(group_uid)
+
+
+@router.post("/groups/{groupUID}/cart/items")
+async def add_item_to_cart(item_dto: CartItemAddDTO, group_uid: str = Path(..., alias="groupUID")):
+    return cart_service.add_cart_item(group_uid, item_dto)
+
+
+@router.get("/groups/{groupUID}/cart/items", response_model=List[CartItemAddDTO])
+async def get_cart_items(group_uid: str = Path(..., alias="groupUID")):
+    return cart_service.get_cart_items(group_uid)
---
+++ shopping/domain/cart_items/repository.py
@@ -0,0 +1,22 @@
+from uuid import uuid4
+
+from shopping.core.types import UNDEFINED
+from shopping.domain.cart_items.entity import CartItemEntity
+from shopping.extensions.database import Repository
+
+
+class CartItemRepository(Repository):
+    def query(self, group_uid: str, item_uid: str = UNDEFINED, unit: str = UNDEFINED):
+        filters = [CartItemEntity.group_uid == group_uid]
+        if item_uid != UNDEFINED:
+            filters.append(CartItemEntity.item_uid == item_uid)
+        if unit != UNDEFINED:
+            filters.append(CartItemEntity.unit == unit)
+        return self.session.query(CartItemEntity).filter(*filters)
+
+    def get(self, group_uid: str, item_uid: str, unit: str):
+        item = self.query(group_uid, item_uid=item_uid, unit=unit).first()
+        if item is None:
+            item = CartItemEntity(uid=uuid4().hex, group_uid=group_uid, item_uid=item_uid, unit=unit)
+            self.session.add(item)
+        return item
